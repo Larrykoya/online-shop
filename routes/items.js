aa326de7bcd66cd0f3ee8e1d399c8608c643c430
larrykoya <larrykoya@yahoo.com>
2023-03-15 00:51:27 +0100

    new product api post set up

--- routes/items.js
+++ routes/items.js
@@ -2,9 +2,12 @@
 const express = require("express");
 const Router = express.Router();
 const path = require("path");
-const mongoose = require("mongoose");
+const fs = require("fs");
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
+const Item = require("../model/items.model");
+express().use(express.urlencoded({ extended: true }));
+express().use(express.json());
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -20,9 +23,32 @@ const storage = multer.diskStorage({
   },
 });
 
+const upload = multer({ storage });
+
 Router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/items.html"));
 });
+
+Router.post("/", upload.single("productImage"), async (req, res) => {
+  try {
+    const imagePath = req.file.path;
+    const image = await cloudinary.uploader.upload(imagePath);
+    // const { name, price, description, alt } = req.body;
+    // const newProduct = await Item.create({
+    //   name,
+    //   price,
+    //   description,
+    //   image: image.url,
+    //   alt,
+    // });
+    // res.redirect(303, "/items");
+    fs.unlinkSync(imagePath);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Unable to add product" });
+  }
+});
+
 Router.get("/new", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/newItem.html"));
 });
