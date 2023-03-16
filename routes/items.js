f62d558558190be2618be0e1e29168c09f2fe462
larrykoya <larrykoya@yahoo.com>
2023-03-16 09:31:04 +0100

    admin page set up

--- routes/items.js
+++ routes/items.js
@@ -4,6 +4,9 @@ const path = require("path");
 Router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/items.html"));
 });
+Router.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/admin.items.html"));
+});
 
 Router.get("/new", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/newItem.html"));
