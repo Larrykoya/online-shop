831521a3f326cafcdd474d64eb45dc5e3ccdf80f
larrykoya <larrykoya@yahoo.com>
2023-03-19 13:18:16 +0100

    added remove item button

--- public/cart.js
+++ public/cart.js
@@ -22,11 +22,6 @@ onload = (event) => {
     return accumulator + object.quantity * object.price;
   }, 0);
   totalContainer.innerHTML = `Total: $${total}`;
-  // if (total < 1000) {
-  //   deliveryFee.innerHTML = `Delivery fee: $${total * 0.1}`;
-  // } else {
-  //   deliveryFee.innerHTML = `Delivery is free.`;
-  // }
 };
 if (cart) {
   checkoutOptions.innerHTML = `
