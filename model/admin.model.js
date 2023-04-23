321784ad52dec41ffe011731c18084c29871b54c
larrykoya <larrykoya@yahoo.com>
2023-04-23 13:56:38 +0100

    admin model

---
+++ model/admin.model.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+adminSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const Admin = model("Admin", adminSchema);
+module.exports = Admin;
