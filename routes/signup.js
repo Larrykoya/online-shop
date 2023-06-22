878e1a0720fd3ddd5d8b0a77bab75f96e1994d08
larrykoya <larrykoya@yahoo.com>
2023-06-22 18:46:48 +0100

    render with view engine

--- routes/signup.js
+++ routes/signup.js
@@ -1,8 +1,7 @@
 const Router = require("express").Router();
-const path = require("path");
 
 Router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/signup.html"));
+  res.render("signup");
 });
 
 module.exports = Router;
