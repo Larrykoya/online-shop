8f2dc58e9e562b5468b361954700a7fb9b05be6f
larrykoya <larrykoya@yahoo.com>
2023-07-03 08:44:19 +0100

    error message handling

--- public/auth.js
+++ public/auth.js
@@ -18,3 +18,24 @@ function logIn(event) {
     })
     .catch((err) => console.log(err));
 }
+
+// function logIn(event) {
+//     event.preventDefault();
+//     let email = event.target[0].value;
+//     let password = event.target[1].value;
+//     fetch("/api/admin/login", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ email, password }),
+//     })
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => {
+//         console.log(data);
+//         //   window.location.href = data.url;
+//       })
+//       .catch((err) => console.log(err));
+//   }
