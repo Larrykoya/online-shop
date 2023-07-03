cedcf300b64451a48cbd1427d99a44c89b52da4a
larrykoya <larrykoya@yahoo.com>
2023-07-03 08:59:32 +0100

    error message handling

--- public/auth.js
+++ public/auth.js
@@ -1,6 +1,6 @@
 let loginError = document.getElementById("login-error-handling");
 let signupError = document.getElementById("signup-error-handling");
-
+loginError.innerHTML = "";
 function logIn(event) {
   event.preventDefault();
   let email = event.target[0].value;
@@ -18,28 +18,36 @@ function logIn(event) {
     .then((data) => {
       console.log(data);
       loginError.innerHTML = "Incorrect credentials!!!";
-      //   window.location.href = data.url;
     })
     .catch((err) => console.log(err));
 }
 
-// function logIn(event) {
-//     event.preventDefault();
-//     let email = event.target[0].value;
-//     let password = event.target[1].value;
-//     fetch("/api/admin/login", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ email, password }),
-//     })
-//       .then((response) => {
-//         return response.json();
-//       })
-//       .then((data) => {
-//         console.log(data);
-//         //   window.location.href = data.url;
-//       })
-//       .catch((err) => console.log(err));
-//   }
+function signup(event) {
+  event.preventDefault();
+  let firstName = event.target[0].value;
+  let lastName = event.target[1].value;
+  let email = event.target[2].value;
+  let password = event.target[3].value;
+  let confirmPassword = event.target[4].value;
+  fetch("/api/admin/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+    }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      signupError.innerHTML = data.message;
+    })
+    .catch((err) => console.log(err));
+}
